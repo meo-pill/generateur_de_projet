968ffbec06b8c52b8a49a4c83133c7e2fadb6aa5
Dark-Gouloum <49821955+Dark-Gouloum@users.noreply.github.com>
2023-09-27 17:25:01 +0200

    Create ConfigGenerateur.sh

---
+++ ConfigGenerateur.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+: '
+	* AUTEUR : Erwan PECHON ; Mewen PUREN
+	* VERSION : 0.1
+	* DATE : Sam. 23 Sept. 2023 16:19:42
+	* Script qui configure le générateur de projet
+'
+
+ligneDossierModele="^dossierModele=\"\$dossierModele\"$"
+
+NouveauProjet=""
+while ( test -z "$NouveauProjet" ) ; do
+	echo -n "Où est le fichier de création de projet ?(q=annuler,.=dossierActu) : "
+	read reponse
+	if ( test "$reponse" == "q" ) then
+		echo "Abandon."
+		exit 0
+	fi
+	reponse="$reponse/Nouveau_projet.sh"
+	if ( test -e "$reponse" ) then
+		NouveauProjet="$reponse"
+	else
+		echo -e "\tLe script \"$reponse\" n'existe pas."
+	fi
+	if ( test $( grep "$ligneDossierModele" | wc -l ) -eq 0 ) then
+		echo "Le fichier \"$NouveauProjet\" est déjà configuré."
+		NouveauProjet=""
+	fi
+done
+
+
+dossierModele=""
+while ( test -z "$dossierModele" ) ; do
+	echo -n "Où est le dossier contenant les modèle de projet ?(q=annuler,.=dossierActu) : "
+	read reponse
+	if ( test "$reponse" == "q" ) then
+		echo "Abandon."
+		exit 0
+	fi
+	if ( test -d "$reponse" ) then
+		if ( test -d "$reponse/type_C" ) then
+			dossierModele=`realpath "$reponse"`
+		else
+			echo -e "\tLe dossier \"$reponse\" existe, mais ne contient pas de dossier de projet type (doit contenir au moins le dossier type de projet en C)."
+		fi
+	else
+		echo -e "\tLe dossier \"$reponse\" n'existe pas."
+	fi
+done
+sed -i -e "s/^dossierModele=\"\$dossierModele\"$/dossierModele=\"$( echo "$dossierModele" | sed "s/\//\\\\\//g" )\"/g" $NouveauProjet
+
+echo -n "Qu'elle est le nom de l'auteur par défault ? : "
+read auteurDefault
+sed -i -e "s/^premierAuteur=\"\$premierAuteur\"$/premierAuteur=\"$auteurDefault\"/g" $NouveauProjet
+
+echo "Fin de la configuration"
