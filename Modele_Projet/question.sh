14f439f9838ec9ddf3f7d59196f13118ac56cb78
meo <83820503+meo-pill@users.noreply.github.com>
2023-11-09 09:31:47 +0100

    refactor(standard): question.sh
    mise a la norme du fichier question

--- Modele_Projet/question.sh
+++ Modele_Projet/question.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-: ' Script servant à poser une question fermé à l utilisateur et à analyser sa réponse
-	paramètre $1 : La question à poser à l utilisateur
+: " Script servant à poser une question fermé à l'utilisateur et à analyser sa réponse
+	paramètre $1 : La question à poser à l'utilisateur
 	paramètre $2 : Un résumer des choix possible
 	autres paramètre : Les possibilité de choix à tester
-		- un groupe est séparer d un autre par le paramètre "--"
-'
+		- un groupe est séparer d un autre par le paramètre '--'
+"
 
 # DÉFINITION DES FONCTIONS
 
 : ' Affiche l aide du script et sort. '
 aide() {
-	if ( test $# -ne 0 -a $1 -eq -1 ) ; then
+	if  test $# -ne 0 -a "$1" -eq -1  ; then
 		echo -e "\n----------------------------------------------\n"
 	fi
 	echo "Affichage de l'aide :"
@@ -20,11 +20,11 @@ aide() {
 	echo -ne "\t- <codePredef> : Un code permettant d'activer un choix prédéfinit. Le code doit faire partie de la liste suivant :"
 	i=0
 	for code in ${!lstStrPredef[*]} ; do
-		if ( test `expr $i % 4` -eq 0 ) ; then
+		if  test $((i % 4)) -eq 0  ; then
 			echo -ne "\n\t\t"
 		fi
 		echo -ne "> '$code'($(echo "${lstStrPredef["$code"]}" | cut -d= -f2))\t"
-		i=`expr $i + 1`
+		i=$((i + 1))
 	done
 	echo ""
 	echo -e "\t- <lettre> : une lettre seulement. La réponse minimal pour choisir ce choix."
@@ -32,25 +32,25 @@ aide() {
 	echo -e "\t- <poss> : Une réponse possible pour choisir ce choix."
 	echo -e "\t- <question> : La question posé à l'utilisateur."
 	echo -e "\t- <poss default> : Qu'est-ce que l'utilisateur répond en ne prenant aucun choix ?."
-	exit $1
+	exit "$1"
 }
 
-: ' Ajoute une réponse prédéfinie
+: " Ajoute une réponse prédéfinie
 	paramètre $1 : Code de la réponse prédéfinie.
 	paramètre $2 : Nom de la réponse prédéfinie.
 	autre paramètre : Les possibilité de réponse.
-	'
+	"
 reponsePredefinie() {
-	repMinimal=`echo $1 | cut -c2-`
+	repMinimal=$(echo "$1" | cut -c2-)
 	code="$1"
 	shift
 	nom="$1"
 	shift
 	lstStrPredef["$code"]="$repMinimal=$nom"
 	i=1
-	for rep in "$repMinimal" "$nom" $* ; do
+	for rep in "$repMinimal" "$nom" "$@" ; do
 		lstPossibilitePredef["$code.$i"]="$rep"
-		i=`expr $i + 1`
+		i=$((i + 1))
 	done
 	return 0
 }
@@ -101,21 +101,21 @@ reponsePredefinie ":n" "non" "no"
 
 # ANALYSE DES PARAMÈTRES
 #	# Vérifié si demande d'aide
-if ( test $# -eq 0 ) ; then
+if test $# -eq 0  ; then
 	aide 0
 fi
-for arg in $* ; do
-	if ( test "$arg" = "-h" -o "$arg" = "--help" ) ; then
+for arg in "$@" ; do
+	if test "$arg" = "-h" -o "$arg" = "--help" ; then
 		aide 0
 	fi
 done
 #	# Ajout des possibilité de demandes et de la question
 indiceChoix=1
-while ( test $# -ne 0 ) ; do
-	if ( test `echo "$1" | cut -c1` = ":" ) ; then
+while test $# -ne 0 ; do
+	if test "$(echo "$1" | cut -c1)" = ":" ; then
 		# Vérifié si le code prédéfinit est déjà utilisé
-		for code in ${lstPredef[*]} ; do
-			if ( test "$code" = "$1" ) ; then
+		for code in "${lstPredef[@]}" ; do
+			if test "$code" = "$1" ; then
 				echo "Le code prédéfinie '$1' à déjà était utilisé."
 				aide -1
 			fi
@@ -123,28 +123,28 @@ while ( test $# -ne 0 ) ; do
 		# Obtenir l'affichage du choix du code prédéfinit
 		strChoix="${lstStrPredef["$1"]}"
 		# Vérifié si le code prédéfinit existe
-		if ( test "$strChoix" = "" ) ; then
+		if test "$strChoix" = ""  ; then
 			echo "Les paramètre commencant par le symbole ':' devrait être un code pour activer un choix possible prédéfinie."
 			aide -1
 		fi
 		# Afficher le choix du code prédéfinit
 		strChoixPossible="$strChoixPossible$strChoix,"
 		# Obtenir les choix possible pour ce choix prédéfinit
-		for nomChoix in `echo "${!lstPossibilitePredef[*]}" | tr " " "\n" | grep "$1\."` ; do
+		for nomChoix in $(echo "${!lstPossibilitePredef[*]}" | tr " " "\n" | grep "$1\.") ; do
 			choix="${lstPossibilitePredef["$nomChoix"]}"
-			if ( test "${lstChoixPossible["$choix"]}" != "" ) ; then
+			if  test "${lstChoixPossible["$choix"]}" != ""  ; then
 				echo "La réponse possible '$choix' à déjà était ajouter."
 				aide -1
 			fi
 			lstChoixPossible["$choix"]="$indiceChoix"
 		done
 		# Sauvegardé le fait d'avoir utilisé ce choix prédéfinit
-		lstPredef=( $lstPredef $1 )
+		lstPredef=( "${lstPredef[@]}" "$1" )
 		# Valider l'utilisation de ce paramètre
 		shift
-	elif ( test "$1" = "-P" ) ; then
+	elif test "$1" = "-P" ; then
 		shift
-		if ( test $# -eq 0 ) ; then
+		if test $# -eq 0 ; then
 			echo "Après une option '-p', il faut donnée au moins le nom du choix et sa réponse minimal."
 			aide -1
 		fi
@@ -152,32 +152,32 @@ while ( test $# -ne 0 ) ; do
 		strChoix="$1"
 		shift
 		# Vérifié l'affichage
-		if ( test `echo "$strChoix" | grep -v "="` ) ; then
+		if test "$(echo "$strChoix" | grep -v "=")" ; then
 			echo "L'affichage du choix devrait avoir un symbole '=' pour séparer le choix minimal de son nom."
 			aide -1
 		fi
-		choixMin=`echo "$strChoix" | cut -d= -f1`
-		choixNom=`echo "$strChoix" | cut -d= -f2-`
-		if ( test `echo "$choixNom" | grep "="` ) ; then
+		choixMin=$(echo "$strChoix" | cut -d= -f1)
+		choixNom=$(echo "$strChoix" | cut -d= -f2-)
+		if test "$(echo "$choixNom" | grep "=")" ; then
 			echo "Le nom du choix ne devrait pas avoir de symbole '=' en son sein."
 			aide -1
 		fi
 		# Afficher le choix du code prédéfinit
 		strChoixPossible="$strChoixPossible$strChoix,"
 		# Obtenir les choix possible pour ce choix prédéfinit
-		if ( test "${lstChoixPossible["$choixMin"]}" != "" ) ; then
+		if test "${lstChoixPossible["$choixMin"]}" != ""  ; then
 			echo "La réponse possible '$choixMin' à déjà était ajouter."
 			aide -1
 		fi
 		lstChoixPossible["$choixMin"]="$indiceChoix"
-		if ( test "${lstChoixPossible["$choixNom"]}" != "" ) ; then
+		if test "${lstChoixPossible["$choixNom"]}" != "" ; then
 			echo "La réponse possible '$choixNom' à déjà était ajouter."
 			aide -1
 		fi
 		lstChoixPossible["$choixNom"]="$indiceChoix"
-		while ( test $# -ne 0 -a `echo $1 | cut -c1` = ">" ) ; do
-			choix="$(echo $1 | cut -c2-)"
-			if ( test "${lstChoixPossible["$choix"]}" != "" ) ; then
+		while test $# -ne 0 -a "$(echo "$1" | cut -c1)" = ">" ; do
+			choix="$(echo "$1" | cut -c2-)"
+			if test "${lstChoixPossible["$choix"]}" != "" ; then
 				echo "La réponse possible '$choix' à déjà était ajouter."
 				aide -1
 			fi
@@ -185,31 +185,31 @@ while ( test $# -ne 0 ) ; do
 			shift
 		done
 	else
-		if ( test "$question" = "" ) ; then
+		if test "$question" = "" ; then
 			question="$1"
-		elif ( test "$strChoixDefault" = "" ) ; then
+		elif test "$strChoixDefault" = "" ; then
 			strChoixDefault="*=$1"
 		else
 			echo "La question et le choix par défault ont déjà était définie."
 			aide -1
 		fi
-		indiceChoix=`expr $indiceChoix - 1`
+		indiceChoix=$((indiceChoix - 1))
 		shift
 	fi
-	indiceChoix=`expr $indiceChoix + 1`
+	indiceChoix=$((indiceChoix + 1))
 done
 #	# Vérifié que La question est était donnée
-if ( test "$question" = "" ) ; then
+if test "$question" = "" ; then
 	echo "Aucune question n'à était posé."
 	aide -1
 fi
 #	# Vérifié qu'au moins une possibilité de réponse est était donnée
-if ( test "$strChoixDefault" = "" -a "${#lstChoixPossible[*]}" = "0" ) ; then
+if test "$strChoixDefault" = "" -a "${#lstChoixPossible[*]}" = "0" ; then
 	echo "L'utilisateur ne peut pas répondre"
 	aide -1
 fi
 #	# Créer la réponse par défault
-if ( test "$strChoixDefault" = "" ) ; then
+if test "$strChoixDefault" = "" ; then
 	strChoixDefault="*=rien"
 fi
 
@@ -219,17 +219,17 @@ echo -n "$question ?($strChoixPossible$strChoixDefault) : "
 
 
 # OBTENIR LA RÉPONSE
-read choix
-monChoix=`echo "$choix" | tr "[[:upper:]]" "[[:lower:]]"`
+read -r choix
+choix="$(echo "$choix" | tr "[:upper:]" "[:lower:]")"
 
 
 # ANALYSER LA RÉPONSE
-if ( test -z "$choix" ) ; then
+if test -z "$choix" ; then
 	exit 0
 fi
 reponse="${lstChoixPossible[$choix]}"
-if ( ! test -z "$reponse" ) ; then
-	exit $reponse
+if ! test -z "$reponse" ; then
+	exit "$reponse"
 fi
 exit 0
 
